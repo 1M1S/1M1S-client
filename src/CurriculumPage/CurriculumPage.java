d529c6b96b905a889a76dfc129adfcda16fadbb8
brenda <brenda524@naver.com>
2021-11-27 22:17:29 +0900

    feat: Add UserCurriculum component(#16)

--- src/CurriculumPage/CurriculumPage.java
+++ src/CurriculumPage/CurriculumPage.java
@@ -40,7 +40,7 @@ public class CurriculumPage extends JFrame {
     };
 
     public static void main(String[] args) {
-       new CurriculumPage();
+        new CurriculumPage();
     }
 
     public CurriculumPage() {
@@ -52,7 +52,7 @@ public class CurriculumPage extends JFrame {
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         add(mainPanel);
-        
+
         mainPanel.setVisible(true);
         mainPanel.setSize(1110, 824);
         mainPanel.setLayout(null);
@@ -70,16 +70,19 @@ public class CurriculumPage extends JFrame {
         });
         curriMainTable.setFont(new Font("나눔고딕", Font.PLAIN, 15));
         //칼럼 만들기
-        String[] header = {"id", "interest","level"};
+        String[] header = {"id", "interest","level","check"};
         curriMainDtm.setColumnIdentifiers(header);
         //id 안 보이게 설정
         curriMainTable.removeColumn(curriMainTable.getColumnModel().getColumn(0));
         curriMainTable.getColumn("interest").setWidth(100);
         curriMainTable.getColumn("interest").setMinWidth(100);
         curriMainTable.getColumn("interest").setMaxWidth(100);
-        curriMainTable.getColumn("level").setWidth(100);
-        curriMainTable.getColumn("level").setMinWidth(100);
-        curriMainTable.getColumn("level").setMaxWidth(100);
+        curriMainTable.getColumn("level").setWidth(130);
+        curriMainTable.getColumn("level").setMinWidth(130);
+        curriMainTable.getColumn("level").setMaxWidth(130);
+        curriMainTable.getColumn("check").setWidth(75);
+        curriMainTable.getColumn("check").setMinWidth(75);
+        curriMainTable.getColumn("check").setMaxWidth(75);
 
         //컬럼 크기, 위치 조절 불가
         curriMainTable.getTableHeader().setReorderingAllowed(false);
@@ -100,9 +103,9 @@ public class CurriculumPage extends JFrame {
         curriSubDtm.setColumnIdentifiers(header2);
         //id 안 보이게 설정
         curriSubTable.removeColumn(curriSubTable.getColumnModel().getColumn(0));
-        curriSubTable.getColumn("Curriculum").setWidth(200);
-        curriSubTable.getColumn("Curriculum").setMinWidth(200);
-        curriSubTable.getColumn("Curriculum").setMaxWidth(200);
+        curriSubTable.getColumn("Curriculum").setWidth(430);
+        curriSubTable.getColumn("Curriculum").setMinWidth(430);
+        curriSubTable.getColumn("Curriculum").setMaxWidth(430);
 
         //컬럼 크기, 위치 조절 불가
         curriSubTable.getTableHeader().setReorderingAllowed(false);
@@ -126,7 +129,7 @@ public class CurriculumPage extends JFrame {
         addCurriBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                addUserCurri(curriMainDtm);
             }
         });
         addCurriBtn.setFont(new Font("나눔고딕", Font.BOLD, 15));
@@ -137,44 +140,18 @@ public class CurriculumPage extends JFrame {
         //일정 삭제 기능
         delCurriBtn.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                int delRow = curriMainTable.getSelectedRow();
-                if (delRow >= 0) {
-                    Long del_member_curriculum_id = (Long)curriMainTable.getModel().getValueAt(delRow, 0);
-
-                    String uri = "http://localhost:8080";
-                    uri = uri + "/api/user/" + Long.toString(1L) + "/curriculum/" + Long.toString(del_member_curriculum_id);
-
-                    try {
-                        HttpClient client = HttpClient.newHttpClient();
-                        HttpRequest request = HttpRequest.newBuilder()
-                                .uri(URI.create(uri))
-                                .DELETE()
-                                .build();
-
-                        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-                        System.out.println("delete response: "+(String)response.body());
-
-                    } catch (Exception ex) {
-                        System.out.println("오류 발생");
-                        ex.printStackTrace();
-                    }
-                    //update
-                    getMainCurri(curriMainDtm);
-                    JOptionPane.showMessageDialog((Component)null, "Row Deleted");
-                } else {JOptionPane.showMessageDialog((Component)null, "Unable To Delete");
-                }
+                delUserCurri(curriMainDtm);
             }
         });
         delCurriBtn.setFont(new Font("나눔고딕", Font.BOLD, 15));
         delCurriBtn.setBounds(899, 128, 125, 35);
         mainPanel.add(delCurriBtn);
 
-
         repaint();
         revalidate();
     }
 
- /******************************************************************************************************************/
+    /******************************************************************************************************************/
     // Main 커리큘럼
 
     // 추가 시 해당 항목 색깔 변경 기능
@@ -189,13 +166,6 @@ public class CurriculumPage extends JFrame {
     //일정 추가 기능 => GET
     void addMainTable(DefaultTableModel dtm) {
         try {
-//            //색깔 변경
-//            int editRow = curriMainTable.getSelectedRow();
-//            if (editRow >= 0) {
-//                //Long editId = (Long)curriMainTable.getModel().getValueAt(editRow, 0);
-//                curriMainTable.set
-//            }
-
             HttpClient client = HttpClient.newHttpClient();
             ObjectMapper objectMapper = new ObjectMapper();
 
@@ -206,9 +176,6 @@ public class CurriculumPage extends JFrame {
             MemberCurriculum addpost = new MemberCurriculum();
 
             //request
-//            objectMapper.registerModule(new JavaTimeModule());
-//            objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-
             String requestBody = objectMapper.writeValueAsString(addpost);
             System.out.println("addMainTable requestBody : " + requestBody);
             HttpRequest request = HttpRequest.newBuilder()
@@ -224,7 +191,7 @@ public class CurriculumPage extends JFrame {
 
             //update
             getMainCurri(dtm);
-            //curriMainTable.firedatachanged;
+            dtm.fireTableDataChanged();
         } catch (Exception ex) {
             System.out.println("오류 발생");
             ex.printStackTrace();
@@ -258,16 +225,17 @@ public class CurriculumPage extends JFrame {
             // Jtable에 일정 추가
             for(Curriculum p : posts) {
                 dtm.addRow(new Object[] {
-                        p.getId(),p.getInterest().getSubject(),p.getLevel()});
+                        p.getId(),p.getInterest().getSubject(),p.getLevel(), false});
                 System.out.println(p);
             }
             dtm.fireTableDataChanged();
+            checkUserCurriculum(dtm);
         } catch (Exception e) {
             System.out.println("오류 발생");
             e.printStackTrace();
         }
     }
-/******************************************************************************************************************/
+    /******************************************************************************************************************/
     // Sub 커리큘럼
 
     // Sub 커리큘럼 읽어오기
@@ -315,44 +283,43 @@ public class CurriculumPage extends JFrame {
         }
     }
 
-    // Sub 커리큘럼 읽어오기
+
 /******************************************************************************************************************/
     // 유저의 커리큘럼
 
     // 유저의 커리큘럼 읽어오기 => GET
-    void getUserCurri(DefaultTableModel dtm) {
-
-        // curriculum_id 가져오기
-        dtm.setRowCount(0);
+    void checkUserCurriculum(DefaultTableModel dtm) {
         try {
             HttpClient client = HttpClient.newHttpClient();
             ObjectMapper mapper = new ObjectMapper();
 
-            //request보내기
-            String uri = "http://localhost:8080/api/user/";
-            uri += Long.toString(1) + "/curriculum";
+            // uri 생성
+            String uri = "http://localhost:8080/api/user/" + Long.toString(1) + "/curriculum";
 
+            // request 생성
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(URI.create(uri))
                     .GET()
                     .build();
-            System.out.println("getUserCurri request : " + request);
 
             // 위에서 생성한 request를 보내고, 받은 response를 저장
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println("getUserCurri response : " + response);
-            System.out.println("getUserCurri body : " + response.body());
 
-            // responseBody to Post Class Array
-            Iterable<MemberCurriculum> posts = mapper.readValue(response.body(), new TypeReference<Iterable<MemberCurriculum>>(){});
+            // responseBody to Iterable<MemberCurriculum>
+            Iterable<MemberCurriculum> M = mapper.readValue(response.body(), new TypeReference<Iterable<MemberCurriculum>>(){});
 
-            // curriMainTable에 해당하는 커리큘럼 목록(행) 색깔 바꾸기(새로고침)
-            for(MemberCurriculum p : posts) {
-//                dtm.addRow(new Object[] {
-//                        p.getId(),p.getContent()});
-                System.out.println(p);
+            // MemberCurriculum에서 커리큘럼 id 가져와서 커리큘럼 목록 중에 id 일치하는 친구들 true로 변경.
+            for(MemberCurriculum m : M) {
+                // 모든 커리큘럼 목록들에 대해서 for문 돌려서 일치하는 id 검색
+                System.out.println("row 개수 : " + dtm.getRowCount());
+                for(int i=0; i<dtm.getRowCount(); i++) {
+                    Long CurriId = (Long) curriMainTable.getModel().getValueAt(i, 0);
+                    if(CurriId.equals(m.getCurriculum().getId())) {
+                        curriMainTable.getModel().setValueAt(true, i, 3);
+                        break;
+                    }
+                }
             }
-            dtm.fireTableDataChanged();
         } catch (Exception e) {
             System.out.println("오류 발생");
             e.printStackTrace();
@@ -376,9 +343,6 @@ public class CurriculumPage extends JFrame {
                 MemberCurriculum addpost = new MemberCurriculum();
 
                 //request
-//            objectMapper.registerModule(new JavaTimeModule());
-//            objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-
                 String requestBody = mapper.writeValueAsString(addpost);
                 System.out.println("addUserCurri requestBody : " + requestBody);
                 HttpRequest request = HttpRequest.newBuilder()
@@ -392,23 +356,15 @@ public class CurriculumPage extends JFrame {
                 System.out.println("addUserCurri response : " + response);
                 System.out.println("addUserCurri response body : " + response.body());
 
-                // responseBody to Post Class Array
-                Iterable<MemberCurriculum> posts = mapper.readValue(response.body(), new TypeReference<Iterable<MemberCurriculum>>(){});
-
-                // 커리큘럼 id 비교
-                for(MemberCurriculum p : posts) {
-                    if(addCurriId == p.getCurriculum().getId()){
-                        // curriMainTable에 해당하는 커리큘럼 목록(행) 색깔 바꾸기(새로고침)
-
-
-                        break;
-                    }
-                }
+                // 선택한 커리큘럼의 check여부 true로 변경.
+                curriMainTable.getModel().setValueAt(true, addRow,3);
                 dtm.fireTableDataChanged();
 
+                JOptionPane.showMessageDialog((Component)null, "User-Curriculum added");
             } catch (Exception ex) {
                 System.out.println("오류 발생");
                 ex.printStackTrace();
+                JOptionPane.showMessageDialog((Component)null, "Unable To add");
             }
         }
     }
@@ -433,23 +389,23 @@ public class CurriculumPage extends JFrame {
                 HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
                 System.out.println("delUserCurri response: "+(String)response.body());
 
+                // 선택한 커리큘럼의 check여부 false로 변경.
+                curriMainTable.getModel().setValueAt(false, delRow,3);
+
             } catch (Exception ex) {
                 System.out.println("오류 발생");
                 ex.printStackTrace();
             }
-
-            // curriMainTable에 해당하는 커리큘럼 목록(행) 색깔 바꾸기(새로고침)
-
-
             dtm.fireTableDataChanged();
-
             JOptionPane.showMessageDialog((Component)null, "User-Curriculum Deleted");
-        } else {JOptionPane.showMessageDialog((Component)null, "Unable To Delete");
+        } else {
+            JOptionPane.showMessageDialog((Component)null, "Unable To Delete");
         }
     }
 
 
 /******************************************************************************************************************/
+
     class mainCurriculumPanel extends JPanel {
         @Override
         public void paintComponent(Graphics g) {
@@ -458,30 +414,4 @@ public class CurriculumPage extends JFrame {
             g.drawImage(image.getImage(), 0, 0, d.width, d.height, (ImageObserver)null);
         }
     }
-
-//    class MyTableCellRenderer extends DefaultTableCellRenderer {
-//        private static final long serialVersionUID = 1L;
-//        @Override
-//        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-//            Component cell = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-//            if (!isSelected) {
-//                if (column == 0) {
-//                    cell.setForeground(Color.white);
-//                    if (value.equals("CRITICAL")) {
-//                        cell.setBackground(Color.RED);
-//                    } else if (value.equals("MAJOR")) {
-//                        cell.setBackground(Color.ORANGE);
-//                    } else if (value.equals("MINOR")) {
-//                        cell.setBackground(Color.YELLOW);
-//                    } else if (value.equals("WARNING")) {
-//                        cell.setBackground(Color.gray);
-//                    }
-//                } else {
-//                    cell.setBackground(Color.white);
-//                }
-//            }
-//            return this;
-//        }
-//    }
 }
-
